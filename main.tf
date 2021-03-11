a8f8aace6a2b82458ed64d0b6fc1f39c651c9663
Aman Gaur <gaman@athenahealth.com>
2021-03-11 18:58:56 +0530

    creating vpc

--- main.tf
+++ main.tf
@@ -7,11 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
-}
-
 resource "aws_instance" "example" {
   ami = "ami-00f8e2c955f7ffa9b" # i guess its a centos machine. taken from centos.org
   instance_type = "t2.micro"
